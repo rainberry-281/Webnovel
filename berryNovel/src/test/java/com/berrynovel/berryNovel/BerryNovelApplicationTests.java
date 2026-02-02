f0be73ad17be2fdbc98d192f9001139decd7e2ab
rainberry-281 <nguyennhatanh533@gmail.com>
2026-02-02 18:38:56 +0700

    a

---
+++ berryNovel/src/test/java/com/berrynovel/berryNovel/BerryNovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berrynovel.berryNovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerryNovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
