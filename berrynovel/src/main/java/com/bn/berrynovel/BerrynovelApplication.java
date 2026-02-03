b836e90d766224482a6e1d72efcfb8a9f9b155b4
rainberry-281 <nguyennhatanh533@gmail.com>
2026-02-03 18:45:17 +0700

    sz

---
+++ berrynovel/src/main/java/com/bn/berrynovel/BerrynovelApplication.java
@@ -0,0 +1,13 @@
+package com.bn.berrynovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerrynovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerrynovelApplication.class, args);
+	}
+
+}
---
+++ berrynovel/src/test/java/com/bn/berrynovel/BerrynovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bn.berrynovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerrynovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
