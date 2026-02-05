3a1f12dd973caef3fe3c55baea10b64a805b115a
rainberry-281 <nguyennhatanh533@gmail.com>
2026-02-05 07:16:35 +0700

    ss

--- berrynovel/src/main/java/com/bn/berrynovel/BerrynovelApplication.java
+++ berrynovel/src/main/java/com/bn/berrynovel/BerrynovelApplication.java
@@ -2,12 +2,16 @@ package com.bn.berrynovel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BerrynovelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BerrynovelApplication.class, args);
+		ApplicationContext beans = SpringApplication.run(BerrynovelApplication.class, args);
+		for (String s : beans.getBeanDefinitionNames()) {
+			System.out.println(s);
+		}
 	}
 
 }
