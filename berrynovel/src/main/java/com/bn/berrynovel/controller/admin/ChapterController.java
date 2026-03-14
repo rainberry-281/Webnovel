04c30912e93caef28a9ffba132c4d8e45206c1fd
rainberry-281 <nguyennhatanh533@gmail.com>
2026-03-14 22:00:39 +0700

    ss

--- berrynovel/src/main/java/com/bn/berrynovel/controller/admin/ChapterController.java
+++ berrynovel/src/main/java/com/bn/berrynovel/controller/admin/ChapterController.java
@@ -2,7 +2,6 @@ package com.bn.berrynovel.controller.admin;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
